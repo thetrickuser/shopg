893ba5deca05aa4202b827ab2ad7341097eb2339
thetrickuser <coolady1807@outlook.com>
2024-04-23 09:41:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ResponsiveAppBar from "./ReponsiveAppBar";
+import Carousel from "./Carousel";
+
+function App() {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <Carousel />
+    </>
+  );
+}
+
+export default App;
